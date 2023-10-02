a154cf1d2c46f25b6fcdc03b4330adf450cf4dc1
pietrolupi <pietro.lupi92@gmail.com>
2023-10-02 17:07:07 +0200

    [add]: aggiunta stampa dinamica msg sinistra

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ createApp({
     return{
 
       contacts: [
+
+
         {
             name: 'Michele',
-            avatar: './img/avatar_1.png',
+            avatar: './img/avatar_1.jpg',
             visible: true,
             messages: [
                 {
@@ -31,7 +33,7 @@ createApp({
         },
         {
             name: 'Fabio',
-            avatar: './img/avatar_2.png',
+            avatar: './img/avatar_2.jpg',
             visible: true,
             messages: [
                 {
@@ -53,7 +55,7 @@ createApp({
         },
         {
             name: 'Samuele',
-            avatar: './img/avatar_3.png',
+            avatar: './img/avatar_3.jpg',
             visible: true,
             messages: [
                 {
@@ -75,7 +77,7 @@ createApp({
         },
         {
             name: 'Alessandro B.',
-            avatar: './img/avatar_4.png',
+            avatar: './img/avatar_4.jpg',
             visible: true,
             messages: [
                 {
@@ -92,7 +94,7 @@ createApp({
         },
         {
             name: 'Alessandro L.',
-            avatar: './img/avatar_5.png',
+            avatar: './img/avatar_5.jpg',
             visible: true,
             messages: [
                 {
@@ -109,7 +111,7 @@ createApp({
         },
         {
             name: 'Claudia',
-            avatar: './img/avatar_6.png',
+            avatar: './img/avatar_6.jpg',
             visible: true,
             messages: [
                 {
@@ -131,7 +133,7 @@ createApp({
         },
         {
             name: 'Federico',
-            avatar: './img/avatar_7.png',
+            avatar: './img/avatar_7.jpg',
             visible: true,
             messages: [
                 {
@@ -148,7 +150,7 @@ createApp({
         },
         {
             name: 'Davide',
-            avatar: './img/avatar_8.png',
+            avatar: './img/avatar_8.jpg',
             visible: true,
             messages: [
                 {
@@ -168,7 +170,9 @@ createApp({
                 }
             ],
         }
-    ]
+    ],
+
+    last: ''
     
     
     
@@ -179,16 +183,27 @@ createApp({
   },
 
   methods: {
-   
+    
+      getLastMessage(index){
+        last = this.contacts[index].messages.length
+        
+        if(this.contacts[index].messages[last - 1].status === 'sent'){
+
+          return 'Tu: ' + this.contacts[index].messages[last - 1].message
+        }else{
+          return this.contacts[index].name + ': ' + this.contacts[index].messages[last - 1].message
+        }
+      }
+
       
     },
 
-
+    
   
 
   mounted(){
     
     console.log('ciaoooooooooo');
-
+    console.log(this.contacts[1].messages.length);
   }
 }).mount('#app')
